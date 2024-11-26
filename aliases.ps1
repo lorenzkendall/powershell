b7006b5a514ece05200aff4d7f18a1167d8789f9
sudokenzo <150217651+sudokenzo@users.noreply.github.com>
2024-11-26 11:14:21 -0600

    Update aliases.ps1

--- aliases.ps1
+++ aliases.ps1
@@ -5,10 +5,9 @@ Set-Alias h hostname
 Set-Alias i systeminfo
 Set-Alias l ls
 
+function ip {ipconfig /all}
 function n {$colors = @('Black', 'DarkRed', 'DarkGreen', 'DarkYellow', 'DarkBlue', 'DarkMagenta', 'DarkCyan', 'Gray', 'DarkGray', 'Red', 'Green', 'Yellow', 'Blue', 'Magenta', 'Cyan', 'White')
 	foreach ($color in $colors) {Write-Host "This is $color text" -ForegroundColor $color} }
-
-function ip {ipconfig /all}
 function p {ping -t 1.1.1.1}
 function x {exit}
 
