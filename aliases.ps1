72bef522203de5c1eee681a7356dd4ce4d7e7d8a
sudokenzo <150217651+sudokenzo@users.noreply.github.com>
2024-11-26 11:13:11 -0600

    Update aliases.ps1

--- aliases.ps1
+++ aliases.ps1
@@ -1,12 +1,14 @@
 notepad $PROFILE
 
 Set-Alias c cls
+Set-Alias h hostname
+Set-Alias i systeminfo
+Set-Alias l ls
 
 function n {$colors = @('Black', 'DarkRed', 'DarkGreen', 'DarkYellow', 'DarkBlue', 'DarkMagenta', 'DarkCyan', 'Gray', 'DarkGray', 'Red', 'Green', 'Yellow', 'Blue', 'Magenta', 'Cyan', 'White')
-foreach ($color in $colors) {
-    Write-Host "This is $color text" -ForegroundColor $color
-}
-}
+	foreach ($color in $colors) {Write-Host "This is $color text" -ForegroundColor $color} }
 
-function x {exit}
+function ip {ipconfig /all}
 function p {ping -t 1.1.1.1}
+function x {exit}
+
