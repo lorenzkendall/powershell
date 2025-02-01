b10c2dd6e2910b5d66be6df3bab4dde2d14ede4f
sudokenzo <150217651+sudokenzo@users.noreply.github.com>
2025-02-01 08:33:50 -0800

    Update adproperties-user.ps1

--- adproperties-user.ps1
+++ adproperties-user.ps1
@@ -1,1 +1,8 @@
+# To run AD-related PowerShell commands from your local workstation against a remote DC: 
+# Install Remote Server Administration Tools (RSAT) 
+# https://www.microsoft.com/en-us/download/details.aspx?id=45520 
+# Install AD module for PowerShell 
+Import-module ActiveDirectory 
+
 Get-ADUser -Identity user.name -Properties * 
+
