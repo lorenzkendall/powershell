455bee6005f29ff33d2754b2189e56d776e3cac6
sudokenzo <150217651+sudokenzo@users.noreply.github.com>
2025-02-01 08:33:33 -0800

    Update adaudit-users.ps1

--- adaudit-users.ps1
+++ adaudit-users.ps1
@@ -1,3 +1,9 @@
+# To run AD-related PowerShell commands from your local workstation against a remote DC: 
+# Install Remote Server Administration Tools (RSAT) 
+# https://www.microsoft.com/en-us/download/details.aspx?id=45520 
+# Install AD module for PowerShell 
+Import-module ActiveDirectory 
+
 Get-ADUser -Filter * -Properties * 
 | select Name,Enabled,EmailAddress,LastLogonDate,CanonicalName
 | Export-Csv -Path c:\users\$env:USERNAME\Downloads\adusers.csv -NoTypeInformation  
